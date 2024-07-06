bfda8dfbe55cbd4dbe042176fe06365f60f02e76
branden higby <branden@higby.io>
2024-07-06 12:44:18 -0700

    Spruce up design

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 #!/usr/bin/env node
 
-console.log()
-console.log('Branden Higby')
-console.log('https://www.higby.io')
-console.log('https://front-end.social/@higby')
+import boxen from 'boxen'
+import chalk from 'chalk'
+
+let higby = [
+  {
+    title: 'www',
+    href: '      https://www.higby.io'
+  },
+  {
+    title: 'email',
+    href: '    mailto:hi@higby.io'
+  },
+  {
+    title: 'mastodon',
+    href: ' https://front-end.social/@higby'
+  }
+]
+
+console.log(
+  boxen(higby.map(link => `${chalk.bold(link.title)} ${link.href}`).join('\n'), {
+    padding: 1,
+    title: 'higby',
+    titleAlignment: 'left'
+  })
+)
